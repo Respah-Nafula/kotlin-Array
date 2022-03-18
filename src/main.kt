83915d8cdf5fc6c3213040311aa32c6efa435860
Respah-Nafula <nafularespah@gmail.com>
2022-03-18 23:26:39 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var x=arrays(arrayOf(50,15,30,25))
+    println(x)
+    var w=mixedtypes(arrayOf("Naomi",456,98.4,67.8,true))
+    println(w)
+    var n=characters(arrayOf('e','s','r','u','a','w','i'))
+    println(n)
+
+}
+
+fun arrays(numbers:Array<Int>): Int{
+    var product=1
+    numbers.forEach { number->
+        product*=number
+    }
+    return product
+}
+fun mixedtypes(digits:Array<Any>) :Double{
+    var sum=0.0
+    digits.forEach { number->
+        if (number is Int|| number is Double)
+            sum+=number.toString().toDouble()
+
+    }
+    return sum
+}
+fun characters(chars:Array<Char>): Int {
+    var vowels=0
+    chars.forEach { r->
+        if (r=='a'||r=='e'||r=='i' ||r=='o'||r=='u'){
+            vowels++
+        }
+    }
+    return vowels
+
+}
+
